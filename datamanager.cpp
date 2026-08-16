e7b3b82f69b9d847053dbed42ac5075d14d99a3f
Ayaerr123 <aya.errahouti@gmail.com>
2026-08-17 00:08:05 +0100

    complete datamanager functions

--- datamanager.cpp
+++ datamanager.cpp
@@ -1,6 +1,7 @@
 #include <QSqlQuery>
 #include <QSqlError>
 #include "datamanager.h"
+#include <QCryptographicHash>
 
 DataManager::DataManager() {}
 bool DataManager::connect(){
@@ -26,5 +27,79 @@ bool DataManager::connect(){
             return true;
     }
     else
+    return false;
+}
+bool DataManager::get_user(QString name, user &out){
+    QSqlQuery query(db);
+    query.prepare("SELECT id,username,password_hash,role FROM users WHERE username=:name");
+    query.bindValue(":name",name);
+    bool result=query.exec();
+    if(!result){
+        qDebug()<<"get user failed "<<query.lastError().text();
         return false;
+    }
+    if(query.next()){
+        out.set_user_id(query.value("id").toInt());
+        out.set_username(query.value("username").toString());
+        out.set_role(query.value("role").toString());
+        out.set_password(query.value("password_hash").toString());
+        return true;
+    }
+    else
+        return false;
+}
+bool DataManager::save_user(const user & u){
+    QSqlQuery query(db);
+    query.prepare("INSERT INTO users (username,password_hash,role)"
+                  "VALUES(:username,:password_hash,:role) "
+                  "ON CONFLICT(username) DO UPDATE SET "
+                  "password_hash=:password_hash,"
+                  "role=:role"
+                  "");
+    query.bindValue(":username",u.get_username());
+    QString hashed=QCryptographicHash::hash(u.get_password().toUtf8(),QCryptographicHash::Sha256).toHex();
+    query.bindValue(":password_hash",hashed);
+    query.bindValue(":role",u.get_role());
+    bool result=query.exec();
+    if(!result){
+        qDebug()<<"save user failed "<<query.lastError().text();
+        return false;
+    }
+    return true;
+}
+bool DataManager::save_file(const file & f){
+    QSqlQuery query(db);
+    query.prepare("INSERT INTO files (filename,path,owner_id,size)"
+                  "VALUES(:filename,:path,:owner_id,:size"
+                  "");
+    query.bindValue(":filename",f.get_filename());
+    query.bindValue(":path",f.get_path());
+    query.bindValue(":owner_id",f.get_owner_id());
+    bool result=query.exec();
+    if(!result){
+        qDebug()<<"save file failed "<<query.lastError().text();
+        return false;
+    }
+    return true;
+}
+QVector<file> DataManager::get_files(int userId) const{
+    QSqlQuery query(db);
+    query.prepare("SELECT id,filename,path,owner_id,size FROM files WHERE owner_id=:userId");
+    query.bindValue("userId",userId);
+    bool result=query.exec();
+    if(!result){
+        QDebug()<<"get files saved"<<query.lastError().text();
+        return QVector<file>();
+    }
+    QVector<file> files;
+    while(query.next()){
+        file f;
+        f(query.value("id").toInt(),
+          query.value("filename").toString(),
+          query.value("path").toString(),
+          query.value("owner_id").toInt(),
+          query.value("size").toLongLong());
+        files.append(f);
+    }
+    return files;
 }
