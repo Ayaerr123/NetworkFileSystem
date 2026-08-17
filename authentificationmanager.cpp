1d4abb0b900c47bdcfff6f465be229307cee90b9
Ayaerr123 <aya.errahouti@gmail.com>
2026-08-17 12:08:29 +0100

    full AM class

--- authentificationmanager.cpp
+++ authentificationmanager.cpp
@@ -1,3 +1,29 @@
 #include "authentificationmanager.h"
-
-authentificationManager::authentificationManager() {}
+#include<QCryptographicHash>
+authentificationManager::authentificationManager(DataManager& d):data(d){}
+bool authentificationManager::register_user(QString name, QString password, QString role){
+    user newUser(-1,name,password,role);
+    return data.save_user(newUser);
+}
+bool authentificationManager::login(QString name, QString pw){
+    QString hashed=QCryptographicHash::hash(pw.toUtf8(),QCryptographicHash::Sha256).toHex();
+    user foundUser;
+    bool result= data.get_user(name,foundUser);
+    if(!result)
+        return false;
+    return foundUser.get_password()==hashed;
+}
+bool authentificationManager::is_admin(int id){
+    user u;
+    bool result=data.get_user(id,u);
+    if(!result)
+        return false;
+    return u.get_role()=="admin";
+}
+bool authentificationManager::is_client(int id){
+    user u;
+    bool result=data.get_user(id,u);
+    if(!result)
+        return false;
+    return u.get_role()=="client";
+}
--- authentificationmanager.h
+++ authentificationmanager.h
@@ -13,7 +13,6 @@ public:
     bool register_user(QString,QString,QString);//name password role
     bool is_admin(int);//check if admin id
     bool is_client(int);//check if client id
-    authentificationManager();
 };
 
 #endif // AUTHENTIFICATIONMANAGER_H
