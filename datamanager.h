42828458b19aa6afd80323fb22bd1119cd8d9da6
Ayaerr123 <aya.errahouti@gmail.com>
2026-08-17 12:07:37 +0100

    new changes

--- datamanager.h
+++ datamanager.h
@@ -13,11 +13,14 @@ class DataManager
 public:
     DataManager();
     bool connect();
-    user get_user(QString name,user& out);
+    bool get_user(QString name,user& out);
+    bool get_user(int,user&);
     bool save_user(const user&);
     bool save_file(const file&);
     QVector<file> get_files(int)const;//user files int->user id
     bool delete_file(int);//file id
+    bool delete_file(QString);//by name
+    bool delete_file(QString);//by path
 };
 
 #endif // DATAMANAGER_H
